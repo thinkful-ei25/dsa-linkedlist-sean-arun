908b263326da22d28133cdd984403668d6812d91
Arun Seehra <seehra.a@gmail.com>
2018-12-13 10:49:32 -0600

    Eslint

--- Linked-List.js
+++ Linked-List.js
@@ -1,45 +1,45 @@
-'use strict'; 
+'use strict';
 
-class Node { 
-    constructor(value, next = null){ 
-        this.value = value; 
-        this.next = next; 
-    }
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
-class LinkedList { 
-    constructor(){ 
-        this.head = null; 
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(item) {
+    this.head = new Node(item, this.head);
+  }
+
+  insertLast(item) {
+    if (!this.head) {
+      this.head = new Node(item);
+      return;
     }
 
-    insertFirst(item){ 
-        this.head = new Node(item, this.head); 
+    let curr = this.head;
+    while (curr.next) {
+      curr = curr.next;
+    }
+    curr.next = new Node(item);
+  }
+
+  find(item) {
+    let currNode = this.head;
+
+    while (currNode && currNode.value !== item) {
+      currNode = currNode.next;
     }
 
-    insertLast(item){
-        if (!this.head) { 
-            this.head = new Node(item); 
-            return; 
-        }
+    return currNode;
+  }
 
-        let curr = this.head; 
-        while(curr.next){ 
-            curr = curr.next; 
-        }
-        curr.next = new Node(item); 
-    }
-
-    find(item){ 
-        let currNode = this.head; 
-
-        while(currNode && currNode.value !== item){ 
-            currNode = currNode.next; 
-        }
-
-        return currNode; 
-    }
-
+  remove(item) {}
 }
 
-module.exports = LinkedList; 
-
+module.exports = LinkedList;
