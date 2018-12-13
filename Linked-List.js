ef33fa6c365c57daad8c6cf36bcf11ebce814e1a
Sean Nealon <signalflowsean@gmail.com>
2018-12-13 11:47:06 -0500

    insert and find

---
+++ Linked-List.js
@@ -0,0 +1,45 @@
+'use strict'; 
+
+class Node { 
+    constructor(value, next = null){ 
+        this.value = value; 
+        this.next = next; 
+    }
+}
+
+class LinkedList { 
+    constructor(){ 
+        this.head = null; 
+    }
+
+    insertFirst(item){ 
+        this.head = new Node(item, this.head); 
+    }
+
+    insertLast(item){
+        if (!this.head) { 
+            this.head = new Node(item); 
+            return; 
+        }
+
+        let curr = this.head; 
+        while(curr.next){ 
+            curr = curr.next; 
+        }
+        curr.next = new Node(item); 
+    }
+
+    find(item){ 
+        let currNode = this.head; 
+
+        while(currNode && currNode.value !== item){ 
+            currNode = currNode.next; 
+        }
+
+        return currNode; 
+    }
+
+}
+
+module.exports = LinkedList; 
+
